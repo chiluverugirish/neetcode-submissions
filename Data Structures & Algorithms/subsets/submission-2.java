0f8f5113d945a5ebd26d066c28890e0d9db2d1a8
CHILUVERU GIRISH <144055051+chiluverugirish@users.noreply.github.com>
2026-06-19 00:04:38 +0530

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    HashSet<List<Integer>>hs=new HashSet<>();
+    void rec(int[]nums,int i,List<Integer>l){
+        if(i==nums.length){
+            hs.add(new ArrayList<>(l));
+            return;
+        }
+        rec(nums,i+1,l);
+        l.add(nums[i]);
+        rec(nums,i+1,l);
+        l.removeLast();
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        rec(nums,0,new ArrayList<>());
+        List<List<Integer>>ans=new LinkedList<>();
+        for(List<Integer> li:hs){
+            ans.add(li);
+        }return ans;
+    }
+}
