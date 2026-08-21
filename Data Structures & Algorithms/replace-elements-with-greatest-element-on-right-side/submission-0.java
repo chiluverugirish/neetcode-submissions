be1349009a031e1fc69c8aff28a01cb65fd4a871
CHILUVERU GIRISH <144055051+chiluverugirish@users.noreply.github.com>
2026-08-22 03:17:38 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int n=arr.length;
+        int num=arr[n-1];
+        int ans[]=new int[n];
+        ans[n-1]=-1;
+        for(int i=n-2;i>=0;i--){
+           ans[i]=num;
+           num=Math.max(num,arr[i]);
+        }
+        return ans;
+    }
+}
