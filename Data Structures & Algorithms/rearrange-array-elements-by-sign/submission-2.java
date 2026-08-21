b5ab26ff2155b10c0eddbd60d579d275580214d3
CHILUVERU GIRISH <144055051+chiluverugirish@users.noreply.github.com>
2026-08-21 14:14:48 +0530

    Add: rearrange-array-elements-by-sign - submission-2

---
+++ Data Structures & Algorithms/rearrange-array-elements-by-sign/submission-2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int arr[]=new int[nums.length];
+        int i=0,j=1;
+        for(int num:nums){
+            if(num>0){arr[i]=num;i+=2;}
+            else {arr[j]=num;j+=2;}
+        }
+        return arr;
+    }
+}
