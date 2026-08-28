9010ffc78aa488367913c2d7ea38db1022b27a98
CHILUVERU GIRISH <144055051+chiluverugirish@users.noreply.github.com>
2026-08-28 22:32:25 +0530

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        char ch[]=new char[word1.length()+word2.length()];
+        int i=0,j=0,k=0;
+        while(i<word1.length()&&j<word2.length()){
+            ch[k++]=word1.charAt(i++);
+            ch[k++]=word2.charAt(j++);
+        } 
+        while(i<word1.length())ch[k++]=word1.charAt(i++);
+        while(j<word2.length())ch[k++]=word2.charAt(j++);
+        return new String(ch);
+    }
+}
