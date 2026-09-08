0f65d4d0b33190e025463b4482a8ca1c6458e983
CHILUVERU GIRISH <144055051+chiluverugirish@users.noreply.github.com>
2026-09-08 22:11:46 +0530

    Add: minimum-number-of-operations-to-move-all-balls-to-each-box - submission-0

---
+++ Data Structures & Algorithms/minimum-number-of-operations-to-move-all-balls-to-each-box/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] minOperations(String boxes) {
+        int n=boxes.length();
+        int ans[]=new int[n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j)continue;
+                if(boxes.charAt(j)=='1')ans[i]+=Math.abs(i-j);
+            }
+        }return ans;
+    }
+}
