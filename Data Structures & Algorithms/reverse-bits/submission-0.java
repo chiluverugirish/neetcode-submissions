9604794b0ccdd1a6d8a60ccf838dbbe34598e3d9
CHILUVERU GIRISH <144055051+chiluverugirish@users.noreply.github.com>
2026-09-15 13:35:09 +0530

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int reverseBits(int n) {
+        int num=0;
+        for(int i=0;i<32;i++){
+            if((n&(1<<i))!=0)num=(num|(1<<31-i));
+        }
+        return num;
+    }
+}
